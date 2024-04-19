d51a21e3de99204aa6a9f88789347abfc38e5a7e
alexbold <djalexbold@gmail.com>
2024-04-19 16:51:50 +0300

    add func

--- src/App.vue
+++ src/App.vue
@@ -43,16 +43,52 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
     <v-main>
+<!--   table   -->
       <v-container>
+        <v-data-table
+            :headers="headers"
+            :items="desserts"
+        ></v-data-table>
+      </v-container>
+
+<!--   card   -->
+      <v-container
+          class="py-8 px-6"
+          fluid
+      >
         <v-row>
           <v-col
-              v-for="n in 24"
-              :key="n"
+              v-for="(dessert, key) in desserts"
+              :key="key"
               cols="4"
           >
-            <v-card height="128"></v-card>
+            <v-card
+                class="mx-auto"
+                height="226"
+            >
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
+                  <v-card-title class="text-h5">
+                    {{ dessert.name }}
+                  </v-card-title>
+
+                  <v-card-subtitle>{{ dessert.description }}</v-card-subtitle>
+
+                  <v-card-actions>
+                    <v-btn small>open</v-btn>
+                  </v-card-actions>
+                </div>
+
+                <v-avatar
+                    class="ma-4"
+                    rounded="0"
+                    size="192"
+                >
+                  <v-img src="./assets/test_img.jpg"></v-img>
+                </v-avatar>
+              </div>
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -69,16 +105,74 @@ export default {
     currentTickerIndex: 0,
     intervalId: null,
     drawer: null,
-    ticker: ['tst text1', 'text test 2', 'hgldfkhgdfkl 3', 'tytytytyty 4', 'sdfrrr_5'],
-    cards: ['Сегодня', 'Вчера', 'на прошлой неделе' , ''],
+    ticker: ['tst text1', 'text test 2', 'hgldfkhgdfkl 3', 'tytytytyty 4', 'With Mother’s Day less than a month away, I found this book to be a timely meditation on modern motherhood. Navigating the complexities of bringing another human into the world and exploring today’s medical advances with a pulse on the mothers and myths that have come before her, Emily C.'],
     links: [
       ['mdi-inbox-arrow-down', 'Upload'],
       ['mdi-send', 'Send'],
       ['mdi-delete', 'Trash'],
       ['mdi-alert-octagon', 'Spam'],
     ],
+    headers: [
+      { text: 'Название', value: 'name' },
+      { text: 'Описание', value: 'description' },
+      { text: 'Категория', value: 'category' },
+      { text: 'Тип', value: 'type' },
+      { text: 'Размер(KB)', value: 'size' },
+      { text: 'Автор', value: 'author' },
+    ],
+    desserts: [
+      {
+        name: 'Frozen Yogurt',
+        description: 'Editor’s Pick: With Mother’s Day less than a month away, I found this book to be a timely meditation on modern motherhood. ',
+        category: 'Художественная литература',
+        type: 'PDF',
+        size: 4520,
+        author: 'user123',
+      },
+      {
+        name: 'Ice cream sandwich',
+        description: 'Navigating the complexities of bringing another human into the world and exploring today’s medical advances with a pulse',
+        category: 'Научная и научно-популярная литература',
+        type: 'PDF',
+        size: 386,
+        author: 'user123',
+      },
+      {
+        name: 'Eclair',
+        description:'I wish I knew how Caoilinn Hughes has managed to write a book of such depth and gravity that is also so gripping and relentlessly funny.',
+        category: 'Справочная литература',
+        type: 'PDF',
+        size: 841,
+        author: 'alexbold',
+      },
+      {
+        name: 'Cupcake',
+        description: 'A nuanced, informative look at the history, development, and future of museums of anthropology and ethnology',
+        category: 'Техническая литература',
+        type: 'DOC',
+        size: 6120,
+        author: 'DeveLOP',
+      },
+      {
+        name: 'Bitter Water Opera',
+        description: 'Exuberant and improbable, Bitter Water Opera is a wonder work of noticing.',
+        category: 'Художественная литература',
+        type: 'DjVu',
+        size: 12869,
+        author: 'DeveLOP',
+      },
+      {
+        name: 'Jelly bean',
+        description: 'sdfhsdjkfsdkjf sdhkfjkdshfkjsd sdjkfhsdjkh',
+        category: 'Другое',
+        type: 'PDF',
+        size: 0.0,
+        author: 0,
+      },
+    ],
   }),
   computed: {
+
     currentTickerItem() {
       return this.ticker.at(this.currentTickerIndex);
     }
@@ -103,7 +197,7 @@ export default {
     },
     timerTick() {
       this.currentTickerIndex = Math.round(
-          Math.random() * (this.ticker.length-1)
+          Math.random() * (this.ticker.length - 1)
       );
     }
   }
