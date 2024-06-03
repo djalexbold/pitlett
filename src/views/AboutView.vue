2614e991dcb74f07b81d260b01536fd714930b47
alexbold <djalexbold@gmail.com>
2024-06-04 01:54:33 +0300

    fix categories

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,92 @@
 <template>
-<h1>Категории литературы</h1>
+  <v-container>
+    <v-breadcrumbs :items="breadcrumb">
+      <template v-slot:item="{ item }">
+        <v-breadcrumbs-item :href="item.id" @click="">
+          {{ item.name }}
+        </v-breadcrumbs-item>
+      </template>
+    </v-breadcrumbs>
 
+    <v-card :loading="false" outlined>
+      <v-tabs
+          class="mb-2"
+          v-model="currentItem"
+      >
+        <v-tab
+
+            v-for="item in items"
+            :key="item.id"
+        >{{ item.title }}
+        </v-tab>
+      </v-tabs>
+
+      <v-tabs-items
+          v-model="currentItem"
+      >
+        <div class="text-cols">
+          <v-tab-item
+              v-for="subItem in subItems"
+              :value="subItem.sub_id - 1"
+          >
+
+            <v-card @click="" flat>
+              <v-card-text >
+                {{ subItem.name }}
+              </v-card-text>
+            </v-card>
+
+
+          </v-tab-item>
+        </div>
+      </v-tabs-items>
+
+    </v-card>
+
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'AboutView',
-
-  components: {
-
+  components: {},
+  data: () => ({
+    breadcrumb: [
+      {name: '111'},
+      {name: '222'},
+      {name: '333sub'}
+    ],
+    currentItem: undefined,
+    items: [
+      {id: 1, title: 'Художественная'},
+      {id: 2, title: 'Научная'},
+      {id: 3, title: 'Справочная'},
+      {id: 4, title: 'Техническая'},
+      {id: 5, title: 'Учебная'},
+    ],
+    subItems: [
+      {id: 1, sub_id: 1, name: 'Детективы, триллеры'},
+      {id: 2, sub_id: 1, name: 'Сатира, юмор'},
+      {id: 3, sub_id: 1, name: 'Ужасы, мистика'},
+      {id: 4, sub_id: 1, name: 'Фантастика, фэнтези'},
+      {id: 5, sub_id: 2, name: 'Самообразование'},
+      {id: 6, sub_id: 2, name: 'Программирование'},
+      {id: 7, sub_id: 3, name: 'Энциклопедии'},
+      {id: 8, sub_id: 4, name: 'Автомобили, наземный транспорт'},
+      {id: 9, sub_id: 5, name: 'Физика, астрономия'},
+    ],
+  }),
+  methods: {
+    addItem(item) {
+      console.log(item)
+    },
   },
 }
 </script>
+<style>
+.text-cols {
+  column-count: 2;
+  column-gap: 10px;
+}
+
+</style>
